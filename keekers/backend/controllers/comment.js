47856dd0e7f64a27a46ead63a6f596f3381d4e14
Carine Tham <ctham010@ucr.edu>
2024-03-13 02:43:14 -0700

    fixed comment.js

--- keekers/backend/controllers/comment.js
+++ keekers/backend/controllers/comment.js
@@ -8,7 +8,24 @@ export const insertComment = ( req, res)=>{
 
     db.query(q, [values], (err,data)=>{
         if (err) return res.status(500).json(err);
-        return res.status(200).json("Comment has been inserted.");
+        //return res.status(200).json("Comment has been inserted.");
+        
+        const r = "SELECT comments.*, users.username FROM comments INNER JOIN users ON comments.userId = users.id WHERE comments.postId = ?";
+        
+        var filter = [req.body.postId];
+
+        db.query(r, filter, (err,data)=>{
+            if (err) return res.status(500).json(err);
+            //res.json(data);
+
+            var result = [];
+
+            for(var i = 0; i < data.length; i++) {
+                result.push({id: data[i].id, description: data[i].description, name: data[i].username, userId: data[i].userId});
+            }
+
+            res.send(JSON.stringify(result));
+        });
     });
 };
 
